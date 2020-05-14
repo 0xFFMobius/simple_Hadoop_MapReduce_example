951dfb0ea82cf0e60366504499dadadff5c05805
Bryce Lakamp <transparentmobius@gmail.com>
2020-05-14 07:39:47 +0000

    lowercase and remove stopwords

--- mapper.py
+++ mapper.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 import sys
+from sklearn.feature_extraction import text
+import string
+#import sklearn
+
+sk_swords = text.ENGLISH_STOP_WORDS
+
+stopwords = set(
+    [s.lower() for s in sk_swords] + #from sklearn
+    [s.lower() for s in nltk_swords] + #from nltk
+#    [s.lower() for s in sp_swords] + #from spacy
+    list(string.punctuation)         #and remove punctuation
+)
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line = line.strip().lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stopwords:
+            print '%s\t%s' % (word, "1")
